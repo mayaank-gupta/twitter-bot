551947677a6625d52648f90a05daf621cec199d4
xMayank <mayankgupta.work@gmail.com>
2023-01-29 10:21:30 +0530

    minor changes

--- index.js
+++ index.js
@@ -1,23 +1,21 @@
-
 let Twit = require("twit");
-let sendNotification = require('./helpers').sendNotification;
+let sendNotification = require("./helpers").sendNotification;
 
 let T = new Twit(require("./config"));
-const safePromise = require('./safe-promise');
+const safePromise = require("./safe-promise");
 
-const timer = ms => new Promise(res => setTimeout(res, ms));
+const timer = (ms) => new Promise((res) => setTimeout(res, ms));
 
 function getTweets() {
   return new Promise((resolve, reject) => {
-
-    const hashTags = ["#datascience", "#javascript", "#ai"];
+    const hashTags = ["#datascience", "#javascript", "#chatgpt", "#ml"];
 
     const random = Math.floor(Math.random() * hashTags.length);
 
     let searchHashtag = {
       q: hashTags[random],
       count: 12,
-      result_type: "recent"
+      result_type: "recent",
     };
 
     T.get("search/tweets", searchHashtag, (err, data) => {
@@ -26,18 +24,17 @@ function getTweets() {
         return reject(err);
       }
       if (data && data.statuses.length) {
-        return resolve(data)
+        return resolve(data);
       } else {
         console.log("No Tweets on the Hashtag: ", searchHashtag.q);
         return resolve(null);
       }
-    })
-  })
+    });
+  });
 }
 
 function postTweet(tweet) {
   return new Promise((resolve, reject) => {
-
     T.post("statuses/retweet/" + tweet.id_str, async (e, res) => {
       if (e) {
         console.log("Cannot Retweet your Tweet!");
@@ -45,14 +42,13 @@ function postTweet(tweet) {
       }
       return resolve(true);
     });
-
-  })
+  });
 }
 
 async function retweet() {
   let [error, data] = await safePromise(getTweets());
 
-  if(error) {
+  if (error) {
     retweet();
   }
 
@@ -61,16 +57,15 @@ async function retweet() {
       let tweet = data.statuses[i];
       let [err, successTweet] = await safePromise(postTweet(tweet));
 
-      if(err) {
+      if (err) {
         await sendNotification("Error");
       }
       await sendNotification(i);
-      await timer(1000 * 180);
+      await timer(1000 * 150);
     }
   } else {
     retweet();
   }
-  
 }
 
 retweet();
